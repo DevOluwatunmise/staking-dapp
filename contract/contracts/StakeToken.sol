d54ae26148de9d661bf33f4b99c31301addb89b5
DevOluwatunmise <tunmisedare@gmail.com>
2024-05-25 12:50:30 +0100

    update

---
+++ contract/contracts/StakeToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StakeToken is ERC20 {
+    constructor(uint initialSupply) ERC20("StakeToken", "STK") {
+        _mint(msg.sender, initialSupply* 10**18);
+    }
+}
